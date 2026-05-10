571c5f1d491042d91c8577169905d4009478270c
nabila <nabillatun.nuha@gmail.com>
2026-05-10 19:43:25 +0700

    perbaikan nama data base

--- config/connect.php
+++ config/connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "db_laboratorium_umpku"; // Nama database yang kita rencanakan
+$db   = "db_laboratorium_umpku";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
