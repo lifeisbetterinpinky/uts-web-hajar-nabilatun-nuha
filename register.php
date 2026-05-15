c06c7ebef78dad53b86998f443be2c822d6b1087
nabila <nabillatun.nuha@gmail.com>
2026-05-15 16:22:16 +0700

    menambahkan fitur lupa password

--- register.php
+++ register.php
@@ -4,9 +4,10 @@ include 'config/koneksi.php';
 if (isset($_POST['register'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
+    $pertanyaan = $_POST['pertanyaan']; 
+    $jawaban    = $_POST['jawaban'];    
     
-    $input = mysqli_query($koneksi, "INSERT INTO users (username, password) VALUES ('$username', '$password')");
+    $input = mysqli_query($koneksi, "INSERT INTO users (username, password, pertanyaan, jawaban) VALUES ('$username', '$password', '$pertanyaan', '$jawaban')");
     
     if ($input) {
         
@@ -38,6 +39,16 @@ if (isset($_POST['register'])) {
                 <label style="display: block; margin-bottom: 5px; font-weight: 600;">Password</label>
                 <input type="password" name="password" class="login-input" placeholder="Create a password" required>
             </div>
+            <div class="form-group" style="text-align: left; margin-bottom: 15px;">
+    <label style="display: block; margin-bottom: 5px; font-weight: 600;">Pertanyaan Keamanan</label>
+    <select name="pertanyaan" class="login-input" style="width: 100%; padding: 10px; border-radius: 20px; border: 1px solid #ddd;" required>
+        <option value="Siapa nama ibu kandungmu?">Siapa nama ibu kandungmu?</option>
+    </select>
+</div>
+<div class="form-group" style="text-align: left; margin-bottom: 20px;">
+    <label style="display: block; margin-bottom: 5px; font-weight: 600;">Jawaban</label>
+    <input type="text" name="jawaban" class="login-input" placeholder="Jawaban kamu" required>
+</div>
             <button type="submit" name="register" class="btn-masuk">Register Now</button>
         </form>
 
